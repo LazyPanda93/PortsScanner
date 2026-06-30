776971ddb0a2498f63b3db04bf1a26044c13575c
LudovicoB <ludob9@hotmail.it>
2026-06-30 16:40:31 +0400

    Implemented basic TCP port scanner

---
+++ PortsScanner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Sockets;
+
+string ipAdd, portInput;
+
+Console.Write("Enter IP address: ");
+ipAdd = Console.ReadLine();
+
+Console.Write("Enter port :");
+portInput = Console.ReadLine();
+
+if(!int.TryParse(portInput, out int port))
+{
+    Console.WriteLine("Invalid port");
+    return;
+}
+
+try
+{
+    TcpClient client = new TcpClient();
+    client.Connect(ipAdd, port);
+
+    Console.WriteLine("Port: " + port + " is open");
+    client.Close();
+}
+catch
+{
+    Console.WriteLine("Port: " + port + " is closed");
+}
